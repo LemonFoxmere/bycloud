3add02a1da35f1376ff7ffe8ba22853e8adc7b01
LemonOrangeWasTaken <reallemonorange@gmail.com>
2022-04-22 18:18:21 -0700

    setted up ts and webpack

---
+++ dist/bundle/app.bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!********************!*\
+  !*** ./src/app.ts ***!
+  \********************/
+
+console.log("loaded");
+
+/******/ })()
+;
+//# sourceMappingURL=app.bundle.js.map
